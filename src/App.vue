16c45276f4b8dbb9f54225d3f7c668cc5ff96e2f
mahmud92542 <mhasan92542@gmail.com>
2020-07-09 13:34:15 +0600

    add all files

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<SearchBar />
+	</div>
+</template>
+
+<script>
+
+import SearchBar from './components/SearchBar';
+
+export default {
+	name: 'App',
+	components: {
+		SearchBar
+	}
+}
+</script>
+
+<style>
+	
+</style> 
